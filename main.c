f8c3b744d68dd517310c8d954396b5a5fb7d2c12
unknown <xorjsghkd1011@gmail.com>
2022-09-16 18:09:04 +0900

    과제

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("First output in master\n");
+    
+    printf("Second output in master\n");
+    
+    printf("Third output in master\n");
+    
+    return 0;
+}
